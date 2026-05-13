8fc8b39a177ca1e4933a8332a4c09356d49718b2
TechAsish1212 <gitasish1212@gmail.com>
2026-05-13 23:08:06 +0530

    feat : add loader . complete to react router dom

--- rectRouter/src/main.jsx
+++ rectRouter/src/main.jsx
@@ -7,7 +7,9 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/ContactUs/Contactus.jsx'
-import User from './User/User.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfo } from './components/Github/Github.jsx'
+
 
 // 1st way to create route
 // const router=createBrowserRouter([
@@ -32,19 +34,29 @@ import User from './User/User.jsx'
 // ])
 
 // 2nd way to create route
-const router=createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
-      <Route path='user/:id' element={<User/>}/>
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:id' element={<User />} />
+      {/* without loader */}
+      {/* <Route path='github' element={<Github/>}/> */}
+      {/* use loader */}
+      <Route
+        loader={githubInfo}
+        path='github'
+        element={<Github />}
+      />
+
+
     </Route>
   )
-) 
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
